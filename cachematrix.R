6eb8790d4a41282d05e636c03fdc3577ff7d3170
unknown <gebmania@gmail.com>
2015-07-26 18:38:28 +0900

    assinment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## make it faster to compute the inverse of matrix using caching the result 
+## in advance. I just modified from the example functions of the assignment.
 
-## Write a short comment describing this function...
-## try
-
+# create special matrix that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
 }
 
 
-## Write a short comment describing this function
-
+# compute the inverse of the matrix that returned by upper function..
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setmean(m)
+        m
 }
+
